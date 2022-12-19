d370dac9abfd904ebb81fe32fbcc1b6117519ab3
Wilcascopr <97641210+Wilcascopr@users.noreply.github.com>
2022-12-19 08:08:30 -0500

    version to deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-axios.defaults.baseURL = process.env.PORT || 'http://localhost:5000';
+axios.defaults.baseURL = process.env.BASE_URL || 'http://localhost:5000';
 
 export default axios
